7bb922ad5c313b3232ab9a3682883ba148b209b2
Alex Alexandre <alexalexandre@MBP-de-Alex.bbrouter>
2020-08-20 20:24:39 -0300

    Big Bang

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Index = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Hello Word</Text>
+    </View>
+  );
+};
+
+export default Index;
