f78665568fa8a8015f69bdf85a09fb4087da1b97
Alex Alexandre <alexalexandrejr@gmail.com>
2020-08-20 21:25:43 -0300

    [ADD] - custom fonts, babel root import config

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import TextStyled from './components/TextStyled';
 
 const Index = () => {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Hello Word</Text>
+      <TextStyled h1>Hello Word</TextStyled>
+      <TextStyled>Hello Word</TextStyled>
     </View>
   );
 };
